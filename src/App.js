7e1b764a450f2c7d824c05a9a11bc70306c7f960
nraskind <nraskind@umich.edu>
2022-08-11 01:42:46 -0700

    Added favicons for socials

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span>
+          <a href="https://github.com/nraskind/" target="_blank">
+            <FontAwesomeIcon icon="fa-brands fa-github" size="6x" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/naum-raskind-b83a461b8/"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon="fa-brands fa-linkedin" size="6x" />
+          </a>
+          <a href="mailto:nraskind@umich.edu">
+            <FontAwesomeIcon icon="fa-solid fa-envelope" size="6x" />
+          </a>
+        </span>
+        <h1>Naum Raskind</h1>
       </header>
     </div>
   );
