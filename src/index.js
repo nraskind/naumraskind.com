3561b9e1bf490e07020dcf23e8f8468e3108a95d
nraskind <nraskind@umich.edu>
2022-08-11 01:41:32 -0700

    Added global favicon imports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+/*
+ FONT AWESOME
+*/
+import { library } from "@fortawesome/fontawesome-svg-core";
+/*
+ EACH ADDITIONAL ICON GOES HERE
+ ADD TO THE IMPORT AND LIBRARY STATEMENTS
+*/
+import { fab, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+library.add(fab, faGithub, faLinkedin, faEnvelope);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
     <App />
