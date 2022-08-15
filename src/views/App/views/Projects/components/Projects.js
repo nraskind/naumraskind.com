00321dc41bf31daf8a9e5e5e16136b7248b350cf
nraskind <nraskind@umich.edu>
2022-08-15 16:45:26 -0400

    Added project timeline

--- src/views/App/views/Projects/components/Projects.js
+++ src/views/App/views/Projects/components/Projects.js
@@ -1,5 +1,8 @@
 import "./Projects.css";
+import projectList from "./projectList";
+import Timeline from "react-image-timeline";
+import "react-image-timeline/dist/timeline.css";
 
 export default function Projects() {
-  return <h1>Projects</h1>;
+  return <Timeline reverseOrder={true} events={projectList}></Timeline>;
 }
