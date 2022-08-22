8c9aebe0965bd44ea153627ae4e8fc4c11783585
nraskind <nraskind@umich.edu>
2022-08-22 11:07:56 +0300

    Added links to course guide and atlas

--- src/views/App/views/Coursework/components/courseList.js
+++ src/views/App/views/Coursework/components/courseList.js
@@ -4,6 +4,9 @@ const courseList = [
     courseCode: "EECS 484",
     semester: "Fall 2022",
     topics: ["Query languages", "Database design", "Query optimization"],
+    courseGuide:
+      "https://www.lsa.umich.edu/cg/cg_detail.aspx?content=2410EECS484001&termArray=f_22_2410#Description:~:text=Jagadish%2C%20H%20V-,Description,-Concepts%20and%20methods",
+    atlas: "https://atlas.ai.umich.edu/course/EECS%20484/",
   },
   {
     courseName: "Introduction to Computer Security",
@@ -14,6 +17,9 @@ const courseList = [
       "Building, using, and managing secure systems",
       "Computer forensics",
     ],
+    courseGuide:
+      "https://www.lsa.umich.edu/cg/cg_detail.aspx?content=2410EECS388001&termArray=f_22_2410#:~:text=Ensafi%2C%20Roya-,Description,-This%20course%20introduces",
+    atlas: "https://atlas.ai.umich.edu/course/EECS%20388/",
   },
   {
     courseName: "Web Systems",
@@ -25,6 +31,9 @@ const courseList = [
       "Threading and parallelization",
     ],
     projects: ["Google search engine clone", "Instagram clone (react, flask)"],
+    courseGuide:
+      "https://www.lsa.umich.edu/cg/cg_detail.aspx?content=2370EECS485001&termArray=w_22_2370#:~:text=Kloosterman%2CJohn%20Sidney-,Description,-Concepts%20surrounding%20web",
+    atlas: "https://atlas.ai.umich.edu/course/EECS%20485/",
   },
   {
     courseName: "Machine Learning",
@@ -36,6 +45,9 @@ const courseList = [
       "Python libraries: numpy, pytorch, and sklearn",
     ],
     projects: ["Reddit post sentiment classifier", "Dog breed classifier"],
+    courseGuide:
+      "https://www.lsa.umich.edu/cg/cg_detail.aspx?content=2370EECS445001&termArray=w_22_2370#:~:text=Kutty%2C%20Sindhu-,Description,-Theory%20and%20implementation",
+    atlas: "https://atlas.ai.umich.edu/course/EECS%20445/",
   },
   {
     courseName: "Introduction to Computer Organization",
@@ -51,6 +63,9 @@ const courseList = [
       "Cache simulator",
       "Multiplication and combination in assembly",
     ],
+    courseGuide:
+      "https://www.lsa.umich.edu/cg/cg_detail.aspx?content=2360EECS370001&termArray=f_21_2360#:~:text=Tang%2CLingjia-,Description,-Course%20Overview%0AThis",
+    atlas: "https://atlas.ai.umich.edu/course/EECS%20370/",
   },
   {
     courseName: "Theory of Computation",
@@ -61,6 +76,9 @@ const courseList = [
       "Probability and cryptography",
       "Decidability and polynomial time computability",
     ],
+    courseGuide:
+      "https://www.lsa.umich.edu/cg/cg_detail.aspx?content=2360EECS376001&termArray=f_21_2360#:~:text=Lee%2CEuiwoong-,Description,-Introduction%20to%20theory",
+    atlas: "https://atlas.ai.umich.edu/course/EECS%20376/",
   },
   {
     courseName: "Data Structures and Algorithms",
@@ -76,6 +94,9 @@ const courseList = [
       "Stock market simulator",
       "C++ SQL emulator",
     ],
+    courseGuide:
+      "https://www.lsa.umich.edu/cg/cg_detail.aspx?content=2330EECS281100&termArray=sp_21_2330#:~:text=Paoletti%2CDavid%20R-,Description,-Introduction%20to%20algorithm",
+    atlas: "https://atlas.ai.umich.edu/course/EECS%20281/",
   },
 ];
 
