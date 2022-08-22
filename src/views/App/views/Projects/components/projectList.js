60b7fa2a81fca0869b47cb7dfd8ae80810b9c8d2
nraskind <nraskind@umich.edu>
2022-08-22 07:17:26 +0300

    Added Instagram logo and github links

--- src/views/App/views/Projects/components/projectList.js
+++ src/views/App/views/Projects/components/projectList.js
@@ -2,13 +2,17 @@ import reactLogo from "@/assets/projects/reactLogo.png";
 import covid from "@/assets/projects/covid.png";
 import euchre from "@/assets/projects/euchre.png";
 import google from "@/assets/projects/google.png";
+import instagram from "@/assets/projects/instagram.png";
 
 const projectList = [
   {
     date: new Date(2022, 7),
-    text: "Created this website using React JS",
+    text: "Created this website using React JS. Utilized multiple open source components such as the timeline, sidebar, and bootstrap cards.",
     title: "naumraskind.com",
     imageUrl: reactLogo,
+    buttonText: "Github Link",
+    onClick: () =>
+      window.open("https://github.com/nraskind/naumraskind.com", "_blank"),
   },
   {
     date: new Date(2022, 6),
@@ -17,16 +21,25 @@ const projectList = [
     imageUrl: euchre,
   },
   {
-    date: new Date(2021, 9),
+    date: new Date(2021, 10),
     text: "Built an R Shiny app to scrape data from CDC datasets weekly and present results in a form convenient for analysis with an easy-to-navigate web interface",
     title: "Covid 19 Data Visualizer",
     imageUrl: covid,
+    buttonText: "Github Link",
+    onClick: () =>
+      window.open("https://github.com/nraskind/Covid19-visualizer", "_blank"),
   },
   {
-    date: new Date(2021, 9),
+    date: new Date(2021, 11),
     text: "Created a simple clone of the google search engine that uses PageRank and tf-idf to rank search results",
     title: "Google Search Engine Clone",
     imageUrl: google,
+  },
+  {
+    date: new Date(2021, 9),
+    text: "Created an Instagram clone using a flask backend with a react frontend and a sqlite database",
+    title: "Instagram Clone",
+    imageUrl: instagram,
   },
 ];
 
