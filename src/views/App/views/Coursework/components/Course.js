cb73207a2887a97f8508ae4855722b7e99066fe8
nraskind <nraskind@umich.edu>
2022-08-22 11:07:20 +0300

    Added footer buttons to course cards

--- src/views/App/views/Coursework/components/Course.js
+++ src/views/App/views/Coursework/components/Course.js
@@ -3,15 +3,34 @@ import Col from "react-bootstrap/Col";
 import Tooltip from "react-bootstrap/Tooltip";
 import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "bootstrap/dist/css/bootstrap.css";
 import "./Course.css";
 
 export default function Course(props) {
-  const { courseName, courseCode, semester, topics, projects } = props;
+  const {
+    courseName,
+    courseCode,
+    semester,
+    topics,
+    projects,
+    courseGuide,
+    atlas,
+  } = props;
 
   const showCourseCode = (props) => <Tooltip {...props}>{courseCode}</Tooltip>;
+
+  const showCourseGuideEntry = (props) => (
+    <Tooltip {...props}>
+      {semester} entry for {courseCode} in the UM LSA course guide
+    </Tooltip>
+  );
+
+  const showAtlasEntry = (props) => (
+    <Tooltip {...props}>{courseCode} UM atlas entry</Tooltip>
+  );
 
   const listCourseSection = (courseSectionTitle, courseSectionContainer) => (
     <ListGroup variant="flush">
@@ -22,6 +41,14 @@ export default function Course(props) {
         <ListGroup.Item key={entryIndex}>{courseSectionEntry}</ListGroup.Item>
       ))}
     </ListGroup>
+  );
+
+  const footerButton = (buttonLink, icon, buttonTitle, buttonOverlay) => (
+    <OverlayTrigger placement="top" overlay={buttonOverlay}>
+      <Button className="m-1" href={buttonLink} target="_blank" variant="light">
+        <FontAwesomeIcon icon={`fas ${icon}`} size="xs" /> {buttonTitle}
+      </Button>
+    </OverlayTrigger>
   );
 
   return (
@@ -48,6 +75,16 @@ export default function Course(props) {
           {topics && listCourseSection("Topics", topics)}
           {projects && listCourseSection("Projects", projects)}
         </Card.Body>
+        <Card.Footer>
+          {footerButton(
+            courseGuide,
+            "fa-book",
+            "Course Guide",
+            showCourseGuideEntry
+          )}
+
+          {footerButton(atlas, "fa-atlas", "Atlas", showAtlasEntry)}
+        </Card.Footer>
       </Card>
     </Col>
   );
